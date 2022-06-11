6ec4f6227d8b55182c048e0ec78ac16259869a98
HauntTheHouse <romadurda2001@gmail.com>
2022-06-11 13:07:25 +0300

    Updated build script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#git submodule init
-#git submodule update
+git submodule update --init
 
 cmake -S . -B build/ -G "$1" -DBUILD_UNIT_TESTS=OFF -DBUILD_EXTRAS=OFF -DBUILD_BULLET2_DEMOS=OFF -DASSIMP_BUILD_TESTS=OFF -DASSIMP_INSTALL=OFF -DBUILD_SHARED_LIBS=OFF -DBUILD_OBJECT_LIBS=OFF
+cmake --build build/ --target CoinToss
